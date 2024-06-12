28b7e3817165d86b74c70f71c06a56c89cfad899
LeopoldHsing <LeopoldHsing@gmail.com>
2024-06-12 16:36:18 -0500

    feat: add new util class for Base64 encoding

---
+++ common/common-utils/src/main/java/com/leopoldhsing/horizon/common/utils/Base64Util.java
@@ -0,0 +1,16 @@
+package com.leopoldhsing.horizon.common.utils;
+
+import java.util.Base64;
+
+public class Base64Util {
+
+    public static String encode(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    public static byte[] decode(String data) {
+        return Base64.getDecoder().decode(data);
+    }
+
+    private Base64Util() {}
+}
