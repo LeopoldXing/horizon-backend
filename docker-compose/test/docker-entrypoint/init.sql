55c75997fa820c76e9fc4c98085ca4c642ff801d
LeopoldHsing <LeopoldHsing@gmail.com>
2024-06-23 21:00:16 -0500

    fix: fix the error of deploying the application in test env

---
+++ docker-compose/test/docker-entrypoint/init.sql
@@ -0,0 +1,221 @@
+CREATE DATABASE IF NOT EXISTS `horizon` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION = 'N' */;
+
+USE `horizon`;
+
+
+-- horizon.categories definition
+CREATE TABLE IF NOT EXISTS `categories`
+(
+    `id`               bigint                                                        NOT NULL AUTO_INCREMENT,
+    `category_name`    varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `description`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `category_code`    varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `created_at`       datetime                                                      DEFAULT NULL,
+    `created_by`       varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `last_modified_at` datetime                                                      DEFAULT NULL,
+    `last_modified_by` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+
+-- horizon.country_code definition
+CREATE TABLE IF NOT EXISTS `country_code`
+(
+    `id`               bigint NOT NULL AUTO_INCREMENT,
+    `code`             varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `country_name`     varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `created_at`       datetime                                                      DEFAULT NULL,
+    `created_by`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `last_modified_at` datetime                                                      DEFAULT NULL,
+    `last_modified_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `country_code_unique` (`code`),
+    UNIQUE KEY `country_code_unique_1` (`country_name`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+
+-- horizon.plaid_customers definition
+CREATE TABLE IF NOT EXISTS `plaid_customers`
+(
+    `id`               bigint NOT NULL AUTO_INCREMENT,
+    `shareable_url`    varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `phone_number`     varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `created_at`       datetime                                                      DEFAULT NULL,
+    `created_by`       varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `last_modified_at` datetime                                                      DEFAULT NULL,
+    `last_modified_by` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+
+-- horizon.dwolla_customer definition
+CREATE TABLE IF NOT EXISTS `dwolla_customer`
+(
+    `id`                  bigint NOT NULL AUTO_INCREMENT,
+    `dwolla_customer_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `customer_type`       varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `customer_status`     varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `created_at`          datetime                                                      DEFAULT NULL,
+    `created_by`          varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `last_modified_at`    datetime                                                      DEFAULT NULL,
+    `last_modified_by`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+
+-- horizon.users definition
+CREATE TABLE IF NOT EXISTS `users`
+(
+    `id`                 bigint NOT NULL AUTO_INCREMENT,
+    `email`              varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `first_name`         varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `last_name`          varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `password`           varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `address`            varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `city`               varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `state`              varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  DEFAULT NULL,
+    `postal_code`        varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `date_of_birth`      date                                                          DEFAULT NULL,
+    `ssn`                varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  DEFAULT NULL,
+    `plaid_customer_id`  bigint                                                        DEFAULT NULL,
+    `dwolla_customer_id` bigint                                                        DEFAULT NULL,
+    `created_at`         datetime                                                      DEFAULT NULL,
+    `created_by`         varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `last_modified_at`   datetime                                                      DEFAULT NULL,
+    `last_modified_by`   varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `users_dwolla_customer_FK` (`dwolla_customer_id`),
+    KEY `users_plaid_customers_FK` (`plaid_customer_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+
+-- horizon.banks definition
+CREATE TABLE IF NOT EXISTS `banks`
+(
+    `id`               bigint                                                        NOT NULL AUTO_INCREMENT,
+    `institution_id`   varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `name`             varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `url`              varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `status`           varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `created_at`       datetime                                                      DEFAULT NULL,
+    `created_by`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `last_modified_at` datetime                                                      DEFAULT NULL,
+    `last_modified_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+
+-- horizon.link_bank_country_code definition
+CREATE TABLE IF NOT EXISTS `link_bank_country_code`
+(
+    `id`               bigint NOT NULL AUTO_INCREMENT,
+    `bank_id`          bigint NOT NULL,
+    `country_code_id`  bigint NOT NULL,
+    `created_at`       datetime                                                      DEFAULT NULL,
+    `created_by`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `last_modified_at` datetime                                                      DEFAULT NULL,
+    `last_modified_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `link_bank_country_code_banks_FK` (`bank_id`),
+    KEY `link_bank_country_code_country_code_FK` (`country_code_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+
+-- horizon.accounts definition
+CREATE TABLE IF NOT EXISTS `accounts`
+(
+    `id`                    bigint                                                        NOT NULL AUTO_INCREMENT,
+    `plaid_account_id`      varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `bank_id`               bigint                                                        DEFAULT NULL,
+    `name`                  varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `official_name`         varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `owner_id`              bigint                                                        NOT NULL,
+    `current_balance`       decimal(10, 2)                                                NOT NULL,
+    `available_balance`     decimal(10, 2)                                                DEFAULT NULL,
+    `limit_balance`         decimal(10, 2)                                                DEFAULT NULL,
+    `iso_currency_code`     varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL,
+    `institution_id`        varchar(19) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  DEFAULT NULL,
+    `shareable_id`          varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `funding_source_url`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `mask`                  varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  DEFAULT NULL,
+    `persistent_account_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `type`                  varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `subtype`               varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `created_at`            datetime                                                      DEFAULT NULL,
+    `created_by`            varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `last_modified_at`      datetime                                                      DEFAULT NULL,
+    `last_modified_by`      varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `accounts_banks_FK` (`institution_id`),
+    KEY `accounts_users_FK` (`owner_id`),
+    KEY `accounts_banks2_FK` (`bank_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+
+-- horizon.transactions definition
+CREATE TABLE IF NOT EXISTS `transactions`
+(
+    `id`                  bigint                                                        NOT NULL AUTO_INCREMENT,
+    `name`                varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `amount`              decimal(10, 2)                                                NOT NULL,
+    `currency`            varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `routing_number`      varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `beneficiary_name`    varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `sender_id`           bigint                                                        DEFAULT NULL,
+    `receiver_id`         bigint                                                        DEFAULT NULL,
+    `date`                date                                                          DEFAULT NULL,
+    `datetime`            datetime                                                      DEFAULT NULL,
+    `authorized_date`     date                                                          DEFAULT NULL,
+    `authorized_datetime` datetime                                                      DEFAULT NULL,
+    `status`              varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `sender_account_id`   bigint                                                        DEFAULT NULL,
+    `receiver_account_id` bigint                                                        DEFAULT NULL,
+    `category_id`         bigint                                                        DEFAULT NULL,
+    `channel`             varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `email`               varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `created_at`          datetime                                                      DEFAULT NULL,
+    `created_by`          varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `last_modified_at`    datetime                                                      DEFAULT NULL,
+    `last_modified_by`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `transactions_banks_FK` (`sender_account_id`),
+    KEY `transactions_banks_FK_1` (`receiver_account_id`),
+    KEY `transactions_users_FK` (`sender_id`),
+    KEY `transactions_users_FK_1` (`receiver_id`),
+    KEY `transactions_categories_FK` (`category_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+
+-- horizon.link_transaction_category definition
+CREATE TABLE IF NOT EXISTS `link_transaction_category`
+(
+    `id`               bigint                                                        DEFAULT NULL,
+    `transaction_id`   bigint                                                        DEFAULT NULL,
+    `category_id`      bigint                                                        DEFAULT NULL,
+    `created_at`       datetime                                                      DEFAULT NULL,
+    `created_by`       varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `last_modified_at` datetime                                                      DEFAULT NULL,
+    `last_modified_by` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    KEY `link_transaction_category_transactions_FK` (`transaction_id`),
+    KEY `link_transaction_category_categories_FK` (`category_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
