12b168d0ae1fa758d570127aa5819f27ce9f22b2
LeopoldHsing <LeopoldHsing@gmail.com>
2024-06-28 22:40:21 -0500

    feat: add ELK Stack docker-compose configuration for test environment

--- eureka-server/src/main/java/com/leopoldhsing/horizon/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/leopoldhsing/horizon/eureka/EurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.leopoldhsing.horizon")
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
